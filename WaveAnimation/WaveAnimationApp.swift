4f47d8ea7c239dc83ef9af7aa68e51a0aced2a95
Lokeswari Satyanarayana <lokeswari.satyanarayana@mutualmobile.com>
2021-08-29 14:20:41 +0530

    Initial Commit

---
+++ WaveAnimation/WaveAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveAnimationApp.swift
+//  WaveAnimation
+//
+//  Created by Lokeswari Satyanarayana on 29/08/21.
+//
+
+import SwiftUI
+
+@main
+struct WaveAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
